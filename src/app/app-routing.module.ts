d04c7ce2bf1b8737782d0ad65c794b092a4cf01f
Jigneshkumar Saliya <jigneshsaliya@gmail.com>
2021-06-17 20:26:50 -0400

    added home, login and sign up page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent},
+      { path: 'login', component: LoginComponent},
+      { path: 'sign-up', component: SignupComponent},
+    ])
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
