835f91df067dbcfa6f7f9ce31aa74f472104657d
Jinal <jinalsutariya2016@gmail.com>
2021-08-07 17:35:51 -0400

    WIP: buyer request info

--- src/app/components/buyer/request-info/request-info.component.ts
+++ src/app/components/buyer/request-info/request-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-request-info',
@@ -9,36 +10,35 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class RequestInfoComponent implements OnInit {
 
   vehicleInfoForm = new FormGroup({
-    VINnumber: new FormControl('', [Validators.required, Validators.maxLength(17), Validators.minLength(17), Validators.pattern('^[A-Z0-9]{1,17}$')]),
+    vinNumber: new FormControl('', [Validators.required, Validators.maxLength(17), Validators.minLength(17), Validators.pattern('^[A-Z0-9]{1,17}$')]),
     year: new FormControl(''),
     make: new FormControl(''),
     model: new FormControl(''),
     partName: new FormControl('', Validators.required),
     partType: new FormControl('', Validators.required),
-});
+  });
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
 
   ngOnInit() {
   }
+  val: string = "";
   
- 
-}
-async function getVin( query: object ): Promise<object> {
-  const url = new URL("https://vin-decoder7.p.rapidapi.com/vin?vin=Jn8AS5MTXBW565232");
-  //url.search = new URLSearchParams( query ).toString();
-  const headers = {
+  async onBlurMethod(event: any){
+    let inputValue = (this.val = event.target.value);
+    console.log(inputValue);
     
-          'x-rapidapi-key': 'd6110475d9msh44a7d3aa9f4f36dp1ace69jsneb8463f67236',//Add the key you got from above here- this is my key
-   'x-rapidapi-host': 'vin-decoder7.p.rapidapi.com' //add the host name from above here
-  };
-  const response = await fetch( url.toString(), {headers} );
-  return await response.json();
+    if ((inputValue.invalid)){
+      console.log("Please provide correct VIN number");
+    
+    }else{
+      // call api here
+      const vinResponse = await this.httpService.get(`vin-decoder?vinNumber=${inputValue}`);
+      console.log(vinResponse);
+    }
+  }   
 }
-const query = {
-  vin: "Jn8AS5MTXBW565232",
-};
-(async () => {
-  const data = await getVin( query );
-  console.log(data)
-})()
+ 
+
+
+  
